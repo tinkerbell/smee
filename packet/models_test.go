d41f71620d36536f9e425f9121eafe07c93e1fd0
Manuel Mendez <mmendez534@gmail.com>
2020-07-08 15:03:50 -0400

    packet: Run table driven tests as sub-tests.
    This way we can have nicer test output as it gets split off for each
    row/sub-test and we don't need to call `t.Log(name)` to fake it. We are
    also able to filter sub-tests to run without needing to mess with the
    test file.

--- packet/models_test.go
+++ packet/models_test.go
@@ -19,15 +19,16 @@ func TestNewDiscoveryCacher(t *testing.T) {
 	os.Unsetenv("DATA_MODEL_VERSION")
 
 	for name, test := range cacherTests {
-		t.Log(name)
-		d, err := NewDiscovery([]byte(test.json))
-		if err != nil {
-			t.Fatal("NewDiscovery", err)
-		}
-		dc := (*d).(*DiscoveryCacher)
-		if dc.PrimaryDataMAC().String() != test.primaryDataMac {
-			t.Fatalf("unexpected primary data mac, want: %s, got: %s", test.primaryDataMac, dc.PrimaryDataMAC())
-		}
+		t.Run(name, func(t *testing.T) {
+			d, err := NewDiscovery([]byte(test.json))
+			if err != nil {
+				t.Fatal("NewDiscovery", err)
+			}
+			dc := (*d).(*DiscoveryCacher)
+			if dc.PrimaryDataMAC().String() != test.primaryDataMac {
+				t.Fatalf("unexpected primary data mac, want: %s, got: %s", test.primaryDataMac, dc.PrimaryDataMAC())
+			}
+		})
 	}
 }
 
@@ -37,21 +38,22 @@ func TestNewDiscoveryTinkerbell(t *testing.T) {
 	os.Setenv("DATA_MODEL_VERSION", "1")
 
 	for name, test := range tinkerbellTests {
-		t.Log(name)
-		d, err := NewDiscovery([]byte(test.json))
-		if err != nil {
-			t.Fatal("NewDiscovery", err)
-		}
-		dt := (*d).(*DiscoveryTinkerbellV1)
+		t.Run(name, func(t *testing.T) {
+			d, err := NewDiscovery([]byte(test.json))
+			if err != nil {
+				t.Fatal("NewDiscovery", err)
+			}
+			dt := (*d).(*DiscoveryTinkerbellV1)
 
-		mac, err := net.ParseMAC(test.mac)
-		if err != nil {
-			t.Fatal("parse test mac", err)
-		}
+			mac, err := net.ParseMAC(test.mac)
+			if err != nil {
+				t.Fatal("parse test mac", err)
+			}
 
-		if dt.Network.InterfaceByMac(mac).DHCP.IP.Address.String() != test.ip.Address.String() {
-			t.Fatalf("unexpected ip, want: %v, got: %v", test.ip, dt.Network.InterfaceByMac([]byte(test.mac)).DHCP.IP.Address)
-		}
+			if dt.Network.InterfaceByMac(mac).DHCP.IP.Address.String() != test.ip.Address.String() {
+				t.Fatalf("unexpected ip, want: %v, got: %v", test.ip, dt.Network.InterfaceByMac([]byte(test.mac)).DHCP.IP.Address)
+			}
+		})
 	}
 }
 
@@ -61,200 +63,204 @@ func TestNewDiscoveryMismatch(t *testing.T) {
 
 	os.Unsetenv("DATA_MODEL_VERSION")
 	for name, test := range cacherTests {
-		t.Log(name)
-		d, err := NewDiscovery([]byte(test.json))
-		if err != nil {
-			t.Fatal("NewDiscovery", err)
-		}
-		dt, ok := (*d).(*DiscoveryTinkerbellV1)
-		t.Log(dt)
-		if ok {
-			t.Fatalf("unexpected concrete type returned from NewDiscovery: want=%T, got=%T", &DiscoveryCacher{}, dt)
-		}
+		t.Run(name, func(t *testing.T) {
+			d, err := NewDiscovery([]byte(test.json))
+			if err != nil {
+				t.Fatal("NewDiscovery", err)
+			}
+			dt, ok := (*d).(*DiscoveryTinkerbellV1)
+			t.Log(dt)
+			if ok {
+				t.Fatalf("unexpected concrete type returned from NewDiscovery: want=%T, got=%T", &DiscoveryCacher{}, dt)
+			}
+		})
 	}
 
 	os.Setenv("DATA_MODEL_VERSION", "1")
 	for name, test := range tinkerbellTests {
-		t.Log(name)
-		d, err := NewDiscovery([]byte(test.json))
-		if err != nil {
-			t.Fatal("NewDiscovery", err)
-		}
-		dt, ok := (*d).(*DiscoveryCacher)
-		t.Log(dt)
-		if ok {
-			t.Fatalf("unexpected concrete type returned from NewDiscovery: want=%T, got=%T", &DiscoveryTinkerbellV1{}, dt)
-		}
+		t.Run(name, func(t *testing.T) {
+			d, err := NewDiscovery([]byte(test.json))
+			if err != nil {
+				t.Fatal("NewDiscovery", err)
+			}
+			dt, ok := (*d).(*DiscoveryCacher)
+			t.Log(dt)
+			if ok {
+				t.Fatalf("unexpected concrete type returned from NewDiscovery: want=%T, got=%T", &DiscoveryTinkerbellV1{}, dt)
+			}
+		})
 	}
 }
 
 func TestDiscoveryCacher(t *testing.T) {
 	for name, test := range cacherTests {
-		t.Log(name)
-		d := DiscoveryCacher{}
+		t.Run(name, func(t *testing.T) {
+			d := DiscoveryCacher{}
 
-		if err := json.Unmarshal([]byte(test.json), &d); err != nil {
-			t.Fatal(test.mode, err)
-		}
+			if err := json.Unmarshal([]byte(test.json), &d); err != nil {
+				t.Fatal(test.mode, err)
+			}
 
-		mac, err := net.ParseMAC(test.mac)
-		if err != nil {
-			t.Fatal(test.mode, err)
-		}
+			mac, err := net.ParseMAC(test.mac)
+			if err != nil {
+				t.Fatal(test.mode, err)
+			}
 
-		// suggest we leave this here for the time being for ease of test why we're not seeing what we expect
-		t.Logf("MacType: %s", d.MacType(mac.String()))
-		t.Logf("MacIsType=data: %v", d.MacIsType(mac.String(), "data"))
-		t.Logf("primaryDataMac: %s", d.PrimaryDataMAC().HardwareAddr().String())
-		t.Logf("MAC: %v", d.MAC())
-		t.Logf("d.mac: %v", d.mac)
-		t.Logf("mac: %s", mac.String())
-		if d.Instance() != nil {
-			t.Logf("instance: %v", d.Instance())
-			t.Logf("instanceId: %s", d.Instance().ID)
-			t.Logf("instance State: %s", d.Instance().State)
-		}
-		t.Logf("hardware IP: %v", d.hardwareIP())
-		t.Log()
-		h := *d.Hardware()
-		for _, ip := range h.HardwareIPs() {
-			t.Logf("hardware IP: %v", ip)
+			// suggest we leave this here for the time being for ease of test why we're not seeing what we expect
+			t.Logf("MacType: %s", d.MacType(mac.String()))
+			t.Logf("MacIsType=data: %v", d.MacIsType(mac.String(), "data"))
+			t.Logf("primaryDataMac: %s", d.PrimaryDataMAC().HardwareAddr().String())
+			t.Logf("MAC: %v", d.MAC())
+			t.Logf("d.mac: %v", d.mac)
+			t.Logf("mac: %s", mac.String())
+			if d.Instance() != nil {
+				t.Logf("instance: %v", d.Instance())
+				t.Logf("instanceId: %s", d.Instance().ID)
+				t.Logf("instance State: %s", d.Instance().State)
+			}
+			t.Logf("hardware IP: %v", d.hardwareIP())
 			t.Log()
-		}
+			h := *d.Hardware()
+			for _, ip := range h.HardwareIPs() {
+				t.Logf("hardware IP: %v", ip)
+				t.Log()
+			}
 
-		d.SetMAC(mac)
-		mode := d.Mode()
-		if mode != test.mode {
-			t.Fatalf("unexpected mode, want: %s, got: %s", test.mode, mode)
-		}
+			d.SetMAC(mac)
+			mode := d.Mode()
+			if mode != test.mode {
+				t.Fatalf("unexpected mode, want: %s, got: %s", test.mode, mode)
+			}
 
-		if mode == "" {
-			continue
-		}
+			if mode == "" {
+				return
+			}
 
-		if d.PrimaryDataMAC().String() != test.primaryDataMac {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.primaryDataMac, d.PrimaryDataMAC().String())
-		}
-		if d.MAC().String() != test.mac {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.mac, d.MAC().String())
-		}
+			if d.PrimaryDataMAC().String() != test.primaryDataMac {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.primaryDataMac, d.PrimaryDataMAC().String())
+			}
+			if d.MAC().String() != test.mac {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.mac, d.MAC().String())
+			}
 
-		conf := d.GetIP(mac)
-		if conf.Address.String() != test.conf.Address.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Address, conf.Address)
-		}
-		if conf.Netmask.String() != test.conf.Netmask.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Netmask, conf.Netmask)
-		}
-		if conf.Gateway.String() != test.conf.Gateway.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Gateway, conf.Gateway)
-		}
+			conf := d.GetIP(mac)
+			if conf.Address.String() != test.conf.Address.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Address, conf.Address)
+			}
+			if conf.Netmask.String() != test.conf.Netmask.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Netmask, conf.Netmask)
+			}
+			if conf.Gateway.String() != test.conf.Gateway.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.conf.Gateway, conf.Gateway)
+			}
 
-		osie := d.ServicesVersion.Osie
-		if osie != test.osie {
-			t.Fatalf("unexpected osie version, want: %s, got: %s", test.osie, osie)
-		}
+			osie := d.ServicesVersion.Osie
+			if osie != test.osie {
+				t.Fatalf("unexpected osie version, want: %s, got: %s", test.osie, osie)
+			}
+		})
 	}
 }
 
 func TestDiscoveryTinkerbell(t *testing.T) {
 	for name, test := range tinkerbellTests {
-		t.Log(name)
-		d := DiscoveryTinkerbellV1{}
+		t.Run(name, func(t *testing.T) {
+			d := DiscoveryTinkerbellV1{}
 
-		if err := json.Unmarshal([]byte(test.json), &d); err != nil {
-			t.Fatal(test.mode, err)
-		}
+			if err := json.Unmarshal([]byte(test.json), &d); err != nil {
+				t.Fatal(test.mode, err)
+			}
 
-		mac, err := net.ParseMAC(test.mac)
-		if err != nil {
-			t.Fatal("parse test mac", err)
-		}
+			mac, err := net.ParseMAC(test.mac)
+			if err != nil {
+				t.Fatal("parse test mac", err)
+			}
 
-		// suggest we leave this here for the time being for ease of test why we're not seeing what we expect
-		if d.ID != test.id {
-			t.Fatalf("unexpected id, want: %s, got: %s", test.id, d.ID)
-		}
+			// suggest we leave this here for the time being for ease of test why we're not seeing what we expect
+			if d.ID != test.id {
+				t.Fatalf("unexpected id, want: %s, got: %s", test.id, d.ID)
+			}
 
-		t.Logf("d.mac: %s", d.mac)
-		t.Logf("dhcp %v", d.Network.InterfaceByMac(mac).DHCP)
-		t.Log()
-		if d.Network.InterfaceByMac(mac).DHCP.IP.Address.String() != test.ip.Address.String() {
-			t.Fatalf("unexpected ip, want: %v, got: %v", test.ip, d.Network.InterfaceByMac(mac).DHCP.IP)
-		}
-		if d.Network.InterfaceByIp(test.ip.Address).DHCP.MAC.String() != mac.String() {
-			t.Fatalf("unexpected mac, want: %s, got: %s", mac, d.Network.InterfaceByIp(test.ip.Address).DHCP.MAC)
-		}
-		if d.Network.InterfaceByMac(mac).DHCP.Hostname != test.hostname {
-			t.Fatalf("unexpected hostname, want: %s, got: %s", test.hostname, d.Network.InterfaceByMac(mac).DHCP.Hostname)
-		}
-		if int(d.LeaseTime(mac).Seconds()) != test.leaseTime {
-			t.Fatalf("unexpected lease time, want: %d, got: %d", test.leaseTime, d.LeaseTime(mac))
-		}
-		// note the difference between []string(nil) and []string{}; use cmp.Diff to check
-		if !reflect.DeepEqual(d.Network.InterfaceByMac(mac).DHCP.NameServers, test.nameServers) {
-			t.Fatalf("unexpected name servers, want: %v, got: %v", test.nameServers, d.Network.InterfaceByMac(mac).DHCP.NameServers)
-		}
-		if !reflect.DeepEqual(d.Network.InterfaceByMac(mac).DHCP.TimeServers, test.timeServers) {
-			t.Fatalf("unexpected time servers, want: %v, got: %v", test.timeServers, d.Network.InterfaceByMac(mac).DHCP.TimeServers)
-		}
-		if d.Network.InterfaceByMac(mac).DHCP.IP.Gateway.String() != test.ip.Gateway.String() {
-			t.Fatalf("unexpected gateway, want: %s, got: %v", test.ip.Gateway, d.Network.InterfaceByMac(mac).DHCP.IP.Gateway)
-		}
-		if d.Network.InterfaceByMac(mac).DHCP.Arch != test.arch {
-			t.Fatalf("unexpected arch, want: %s, got: %s", test.arch, d.Network.InterfaceByMac(mac).DHCP.Arch)
-		}
-		if d.Network.InterfaceByMac(mac).DHCP.UEFI != test.uefi {
-			t.Fatalf("unexpected uefi, want: %v, got: %v", test.uefi, d.Network.InterfaceByMac(mac).DHCP.UEFI)
-		}
-
-		t.Logf("netboot: %v", d.Network.InterfaceByMac(mac).Netboot)
-		t.Logf("netboot allow_pxe: %v", d.Network.InterfaceByMac(mac).Netboot.AllowPXE)
-		t.Logf("netboot allow_workflow: %v", d.Network.InterfaceByMac(mac).Netboot.AllowWorkflow)
-		t.Logf("netboot ipxe: %v", d.Network.InterfaceByMac(mac).Netboot.IPXE)
-		t.Logf("netboot osie: %v", d.Network.InterfaceByMac(mac).Netboot.Osie)
-		t.Log()
-
-		t.Logf("metadata: %v", d.Metadata)
-		t.Logf("metadata state: %v", d.Metadata.State)
-		t.Logf("metadata bonding_mode: %v", d.Metadata.BondingMode)
-		t.Logf("metadata manufacturer: %v", d.Metadata.Manufacturer)
-		if d.Instance() != nil {
-			t.Logf("instance: %v", d.Instance())
-			t.Logf("instance id: %s", d.Instance().ID)
-			t.Logf("instance state: %s", d.Instance().State)
-		}
-		t.Logf("metadata custom: %v", d.Metadata.Custom)
-		t.Logf("metadata facility: %v", d.Metadata.Facility)
-
-		//t.Logf("hardware IP: %v", d.hardwareIP())
-		t.Log()
-		h := *d.Hardware()
-		for _, ip := range h.HardwareIPs() {
-			t.Logf("hardware IP: %v", ip)
+			t.Logf("d.mac: %s", d.mac)
+			t.Logf("dhcp %v", d.Network.InterfaceByMac(mac).DHCP)
 			t.Log()
-		}
+			if d.Network.InterfaceByMac(mac).DHCP.IP.Address.String() != test.ip.Address.String() {
+				t.Fatalf("unexpected ip, want: %v, got: %v", test.ip, d.Network.InterfaceByMac(mac).DHCP.IP)
+			}
+			if d.Network.InterfaceByIp(test.ip.Address).DHCP.MAC.String() != mac.String() {
+				t.Fatalf("unexpected mac, want: %s, got: %s", mac, d.Network.InterfaceByIp(test.ip.Address).DHCP.MAC)
+			}
+			if d.Network.InterfaceByMac(mac).DHCP.Hostname != test.hostname {
+				t.Fatalf("unexpected hostname, want: %s, got: %s", test.hostname, d.Network.InterfaceByMac(mac).DHCP.Hostname)
+			}
+			if int(d.LeaseTime(mac).Seconds()) != test.leaseTime {
+				t.Fatalf("unexpected lease time, want: %d, got: %d", test.leaseTime, d.LeaseTime(mac))
+			}
+			// note the difference between []string(nil) and []string{}; use cmp.Diff to check
+			if !reflect.DeepEqual(d.Network.InterfaceByMac(mac).DHCP.NameServers, test.nameServers) {
+				t.Fatalf("unexpected name servers, want: %v, got: %v", test.nameServers, d.Network.InterfaceByMac(mac).DHCP.NameServers)
+			}
+			if !reflect.DeepEqual(d.Network.InterfaceByMac(mac).DHCP.TimeServers, test.timeServers) {
+				t.Fatalf("unexpected time servers, want: %v, got: %v", test.timeServers, d.Network.InterfaceByMac(mac).DHCP.TimeServers)
+			}
+			if d.Network.InterfaceByMac(mac).DHCP.IP.Gateway.String() != test.ip.Gateway.String() {
+				t.Fatalf("unexpected gateway, want: %s, got: %v", test.ip.Gateway, d.Network.InterfaceByMac(mac).DHCP.IP.Gateway)
+			}
+			if d.Network.InterfaceByMac(mac).DHCP.Arch != test.arch {
+				t.Fatalf("unexpected arch, want: %s, got: %s", test.arch, d.Network.InterfaceByMac(mac).DHCP.Arch)
+			}
+			if d.Network.InterfaceByMac(mac).DHCP.UEFI != test.uefi {
+				t.Fatalf("unexpected uefi, want: %v, got: %v", test.uefi, d.Network.InterfaceByMac(mac).DHCP.UEFI)
+			}
 
-		d.SetMAC(mac)
-		mode := d.Mode()
-		if mode != test.mode {
-			t.Fatalf("unexpected mode, want: %s, got: %s", test.mode, mode)
-		}
+			t.Logf("netboot: %v", d.Network.InterfaceByMac(mac).Netboot)
+			t.Logf("netboot allow_pxe: %v", d.Network.InterfaceByMac(mac).Netboot.AllowPXE)
+			t.Logf("netboot allow_workflow: %v", d.Network.InterfaceByMac(mac).Netboot.AllowWorkflow)
+			t.Logf("netboot ipxe: %v", d.Network.InterfaceByMac(mac).Netboot.IPXE)
+			t.Logf("netboot osie: %v", d.Network.InterfaceByMac(mac).Netboot.Osie)
+			t.Log()
 
-		if mode == "" {
-			continue
-		}
+			t.Logf("metadata: %v", d.Metadata)
+			t.Logf("metadata state: %v", d.Metadata.State)
+			t.Logf("metadata bonding_mode: %v", d.Metadata.BondingMode)
+			t.Logf("metadata manufacturer: %v", d.Metadata.Manufacturer)
+			if d.Instance() != nil {
+				t.Logf("instance: %v", d.Instance())
+				t.Logf("instance id: %s", d.Instance().ID)
+				t.Logf("instance state: %s", d.Instance().State)
+			}
+			t.Logf("metadata custom: %v", d.Metadata.Custom)
+			t.Logf("metadata facility: %v", d.Metadata.Facility)
 
-		conf := d.GetIP(mac)
-		if conf.Address.String() != test.ip.Address.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Address, conf.Address)
-		}
-		if conf.Netmask.String() != test.ip.Netmask.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Netmask, conf.Netmask)
-		}
-		if conf.Gateway.String() != test.ip.Gateway.String() {
-			t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Gateway, conf.Gateway)
-		}
+			//t.Logf("hardware IP: %v", d.hardwareIP())
+			t.Log()
+			h := *d.Hardware()
+			for _, ip := range h.HardwareIPs() {
+				t.Logf("hardware IP: %v", ip)
+				t.Log()
+			}
+
+			d.SetMAC(mac)
+			mode := d.Mode()
+			if mode != test.mode {
+				t.Fatalf("unexpected mode, want: %s, got: %s", test.mode, mode)
+			}
+
+			if mode == "" {
+				return
+			}
+
+			conf := d.GetIP(mac)
+			if conf.Address.String() != test.ip.Address.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Address, conf.Address)
+			}
+			if conf.Netmask.String() != test.ip.Netmask.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Netmask, conf.Netmask)
+			}
+			if conf.Gateway.String() != test.ip.Gateway.String() {
+				t.Fatalf("unexpected address, want: %s, got: %s", test.ip.Gateway, conf.Gateway)
+			}
+		})
 	}
 }
 
