10dacf0feb48be69c24fa80764ed45e18c2cfe54
Manuel Mendez <mmendez@equinix.com>
2021-01-07 15:00:52 -0500

    Makefile: Store ipxe version and hash in a separate file
    This way we can make the .tar.gz recipe depend on it and will force a
    re-run if version or hash changes.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- Makefile
+++ Makefile
@@ -31,13 +31,11 @@ ifeq ($(origin GOBIN), undefined)
 GOBIN := ${PWD}/bin
 export GOBIN
 endif
-
 ipxe/bindata.go: ipxe/bin/ipxe.efi ipxe/bin/snp-hua.efi ipxe/bin/snp-nolacp.efi ipxe/bin/undionly.kpxe
 	go-bindata -pkg ipxe -o $@ $^
 	gofmt -w $@
 
-ipxev := 18dc73d27edb55ebe9cb13c58d59af3da3bd374b
-ipxeh := 17b6bbad8f0a94b15cdb5470bb60c8c7868759efd89f0ea8111c94eefaaa4c0c665a5b8d9547defeb8caf432a0d9d12e25aa81709b4e5a6055cb416c140b4de7
+include ipxev.mk
 ipxeconfigs := $(wildcard ipxe/ipxe/*.h)
 
 ipxe/bin/ipxe.efi: ipxe/ipxe/build/ipxe-x86_64-efi/ipxe.efi
--- ipxe/ipxe/build.sh
+++ ipxe/ipxe/build.sh
@@ -6,31 +6,30 @@ set -eu
 # apt-get -y --no-install-recommends install build-essential gcc-aarch64-linux-gnu git liblzma-dev
 
 name=$(basename "$1")
-arch=$(dirname "$1")
 version=$2
 short_version="$(echo "$version" | cut -c1-5)"
 
 topdir="ipxe-$version"
-cp *.h "$topdir/src/config/local"
+cp ./*.h "$topdir/src/config/local"
 sed -i '/#define OCSP_CHECK/ d' "$topdir/src/config/crypto.h"
 
 case $name in
 undionly.kpxe)
 	cp "$topdir/src/config/local/general.undionly.h" "$topdir/src/config/local/general.h"
 	make -C "$topdir/src" VERSION_PATCH=255 EXTRAVERSION="+ ($short_version)" bin/undionly.kpxe
-	cp $topdir/src/bin/undionly.kpxe .
+	cp "$topdir/src/bin/undionly.kpxe" .
 	;;
 ipxe.efi)
 	cp "$topdir/src/config/local/general.efi.h" "$topdir/src/config/local/general.h"
 	make -C "$topdir/src" VERSION_PATCH=255 EXTRAVERSION="+ ($short_version)" bin-x86_64-efi/ipxe.efi
-	cp $topdir/src/bin-x86_64-efi/ipxe.efi .
+	cp "$topdir/src/bin-x86_64-efi/ipxe.efi" .
 	;;
 snp.efi)
 	cp "$topdir/src/config/local/general.aarch64-snp-nolacp.h" "$topdir/src/config/local/general.h"
 	# http://lists.ipxe.org/pipermail/ipxe-devel/2018-August/006254.html
 	sed -i '/^WORKAROUND_CFLAGS/ s|^|#|' "$topdir/src/arch/arm64/Makefile"
 	CROSS_COMPILE=aarch64-unknown-linux-gnu- make -C "$topdir/src" VERSION_PATCH=255 EXTRAVERSION="+ ($short_version)" bin-arm64-efi/snp.efi
-	cp $topdir/src/bin-arm64-efi/snp.efi .
+	cp "$topdir/src/bin-arm64-efi/snp.efi" .
 	;;
 *) echo "unknown target: $1" && exit 1 ;;
 esac
