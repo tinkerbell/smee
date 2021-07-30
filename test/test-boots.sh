2c80d0874dcb1259e008f621ca55e212f2fc42b7
Amy Tobey <atobey@equinix.com>
2021-07-30 14:11:55 -0700

    set TRACEPARENT from http headers and sleep at the end
    Adds example code for fetching the TRACEPARENT from boots via HTTP
    header.
    
    Adds a sleep 30000 to the end of the file so it doesn't exit, which
    makes it way easier to connect to the container via docker exec to prod
    at boots with CLI tools.
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- test/test-boots.sh
+++ test/test-boots.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-echo "starting DHCP in 5 seconds"
+sleep_at_start=3
+
+echo "starting DHCP in $sleep_at_start seconds"
+set -x
 
 # useful for debugging sometimes
 #tcpdump -ni eth0 &
 
-sleep 5
+sleep $sleep_at_start
 
 # run a mainstream DHCP client in debug mode
 #dhclient -4 -d
 dhcpcd -d -4 --nobackground --noipv4ll -T
 
+# get the OpenTelemetry traceparent via HTTP header
+TRACEPARENT=`curl -sSIX GET http://192.168.99.42 2>/dev/null |awk '/Traceparent:/{print $2}'`
+export TRACEPARENT
+
 # over time we can add some tests here, including stepping through tftp and http
 # requests to boots
+
+# sleep a long time so you can enter the container with
+# docker exec -ti boots_client_1 /bin/sh
+sleep 30000
