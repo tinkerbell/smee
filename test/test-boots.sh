04c24f153b462039233ce3d42348f8692a00cabb
Amy Tobey <atobey@equinix.com>
2021-08-04 12:01:09 -0700

    update testing environment to enable otel
    Adds the opentelemetry-collector to the docker-compose rig, in debug
    mode by default so it will print all spans and traces to stdout. Wires
    this up through environment variables.
    
    Update scripts to use busybox udhcpc which lets us set arbitrary
    options easier than mainstream clients like dhclinet and dhcpcd. Add a
    script for udhcpc that just prints the environment instead of messing
    with the interface. The /tmp file it writes could be useful in unit
    tests.
    
    Add more docs to most of these files explaining how to use them and what
    they're doing.
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- test/test-boots.sh
+++ test/test-boots.sh
@@ -6,16 +6,25 @@ echo "starting DHCP in $sleep_at_start seconds"
 set -x
 
 # useful for debugging sometimes
-#tcpdump -ni eth0 &
+# tcpdump -ni eth0 &
+# alternatively, only show DHCP and pretty print the packets
+# tcpdump -nvvei eth0 port 67 or port 68 &
 
 sleep $sleep_at_start
 
-# run a mainstream DHCP client in debug mode
+# busybox udhcpc will happily set arbitrary DHCP options and is easy
+# to configure with a custom setup script to call on DHCPOFFER
+#
+# dummy setup script for -s is copied in by Dockerfile
+# send -V PXEClient and option 93 set to 0 to get boots to accept this as a PXE
+# DHCP client
+busybox udhcpc -s /busybox-udhcpc-script.sh -V PXEClient -x 0x5d:0000
+# dhclient & dhcpcd don't seem to have a way to set option 93/0x5d though
 #dhclient -4 -d
-dhcpcd -d -4 --nobackground --noipv4ll -T
+#dhcpcd -d -4 --nobackground --noipv4ll -T -i PXEClient
 
 # get the OpenTelemetry traceparent via HTTP header
-TRACEPARENT=`curl -sSIX GET http://192.168.99.42 2>/dev/null |awk '/Traceparent:/{print $2}'`
+TRACEPARENT=$(curl -sSIX GET http://192.168.99.42 2>/dev/null | awk '/Traceparent:/{print $2}')
 export TRACEPARENT
 
 # over time we can add some tests here, including stepping through tftp and http
