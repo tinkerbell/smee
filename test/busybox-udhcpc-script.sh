0da699985247970d535d09903117bed9cc621ae8
Amy Tobey <atobey@equinix.com>
2021-08-05 13:05:54 -0700

    shfmt
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- test/busybox-udhcpc-script.sh
+++ test/busybox-udhcpc-script.sh
@@ -2,4 +2,4 @@
 # instead of messing with the actual interface configuration
 # this just dumps the environment variables to a file and stdout
 
-env |grep -v '^[A-Z]' | sort |tee /tmp/dhcpoffer-vars.sh
+env | grep -v '^[A-Z]' | sort | tee /tmp/dhcpoffer-vars.sh
