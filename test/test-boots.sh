425ed2d48308feff46d73b1a3bc53ed186db3f35
Amy Tobey <atobey@equinix.com>
2021-08-10 16:24:49 -0700

    add DHCP client GUID to udhcpc command
    Also added inline docs.
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- test/test-boots.sh
+++ test/test-boots.sh
@@ -16,9 +16,16 @@ sleep $sleep_at_start
 # to configure with a custom setup script to call on DHCPOFFER
 #
 # dummy setup script for -s is copied in by Dockerfile
-# send -V PXEClient and option 93 set to 0 to get boots to accept this as a PXE
-# DHCP client
-busybox udhcpc -s /busybox-udhcpc-script.sh -V PXEClient -x 0x5d:0000
+# opt60 (-V PXEClient) pretend to be an Intel PXE client. required to be noticed by boots
+# opt93 (-x 0x5d) set to 0 for "Intel x86PC" platform, required by boots
+# opt97 (-x 0x61) sets the client guid (https://datatracker.ietf.org/doc/html/rfc4578#section-2.3)
+#              first 8 octets should be zeroes to make boots happy (Intel PXE does this)
+#              ID: 4a525bd43517df7f8b4799c18d (randomly generated and hard-coded here)
+busybox udhcpc \
+    -s /busybox-udhcpc-script.sh \
+    -V PXEClient \
+    -x 0x5d:0000 \
+    -x 0x61:000000004a525bd43517df7f8b4799c18d
 
 # the busybox script writes the DHCP variables to /tmp/dhcpoffer-vars.sh
 . /tmp/dhcpoffer-vars.sh
