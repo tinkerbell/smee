a13f662d83cb6a60fc752bba7cd0ea9157010552
Amy Tobey <atobey@equinix.com>
2021-08-17 10:52:29 -0700

    make shellcheck less unhappy
    this code is no more or less correct than it was before
    
    TRACEPARENT is exported now
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- test/extract-traceparent-from-opt43.sh
+++ test/extract-traceparent-from-opt43.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck shell=dash
 
 # extract_traceparent_from_opt43 takes a hex string from busybox udhcpc's opt43
 # and extracts sub-option 69 which is where we stuff the traceparent in binary,
@@ -16,50 +17,57 @@
 #
 # takes 1 argument, usually $opt43
 # sets $opt43x69 to the hex traceparent
-# sets $TRACEPARENT to the W3C-formatted traceparent string
-# neither are exported by default
+# exports $TRACEPARENT to the W3C-formatted traceparent string
 extract_traceparent_from_opt43 () {
-  local hexdata=$1 ; shift
+  local hexdata strlen offset
+  hexdata=$1 ; shift
   opt43x69="" # in case the global is still set, empty it
-  local strlen=$(echo -n "$hexdata" |wc -c)
-  local offset=1 # cut(1) uses offsets starting at 1
+  strlen=$(echo -n "$hexdata" |wc -c)
+  offset=1 # cut(1) uses offsets starting at 1
 
-  while [ $offset -lt $strlen ] ; do
+  while [ "$offset" -lt "$strlen" ] ; do
     # extract the type number, 1 byte
-    local type_end=$((offset + 1))
-    local htype=$(echo -n $hexdata | cut -c "${offset}-${type_end}")
-    local type=$(printf '%d' "0x$htype")
+    local type_end htype type
+    type_end=$((offset + 1))
+    htype=$(echo -n "$hexdata" | cut -c "${offset}-${type_end}")
+    type=$(printf '%d' "0x$htype")
 
     # extract the value length, 1 byte
-    local len_start=$((offset + 2))
-    local len_end=$((offset + 3))
-    local hlen=$(echo -n $hexdata | cut -c "${len_start}-${len_end}")
-    local len=$(printf '%d' "0x$hlen")
+    local len_start len_end hlen len
+    len_start=$((offset + 2))
+    len_end=$((offset + 3))
+    hlen=$(echo -n "$hexdata" | cut -c "${len_start}-${len_end}")
+    len=$(printf '%d' "0x$hlen")
+
+    # calculate value offsets
+    local bov eov
     local bov=$((offset + 4)) # beginning of value
     local eov=$((bov + len * 2 - 1)) # end of value
 
-    if [ $type -eq 69 ] ; then
+    if [ "$type" -eq 69 ] ; then
       # set global to the full tp hex data
-      opt43x69=$(echo -n $hexdata | cut -c "${bov}-${eov}")
+      opt43x69=$(echo -n "$hexdata" | cut -c "${bov}-${eov}")
 
       # break out the sections of the traceparent to make a proper W3C tp string
-      local ver=$(echo -n $opt43x69 | cut -c "1-2")       # 1 byte
-      local trace_id=$(echo -n $opt43x69 | cut -c "3-34") # 16 bytes
-      local span_id=$(echo -n $opt43x69 | cut -c "35-50") # 8 bytes
-      local flags=$(echo -n $opt43x69 | cut -c "51-53")   # 1 byte
+      local ver trace_id span_id flags
+      ver=$(echo -n "$opt43x69" | cut -c "1-2")       # 1 byte
+      trace_id=$(echo -n "$opt43x69" | cut -c "3-34") # 16 bytes
+      span_id=$(echo -n "$opt43x69" | cut -c "35-50") # 8 bytes
+      flags=$(echo -n "$opt43x69" | cut -c "51-53")   # 1 byte
 
       # set TRACEPARENT to the W3C-formatted string
-      TRACEPARENT="${ver}-${trace_id}-${span_id}-${flags}"
+      export TRACEPARENT="${ver}-${trace_id}-${span_id}-${flags}"
     fi
 
     # add to the offset:
     # 4 characters for type and len e.g. 0601 (type 6, length 1)
     # len (is bytes) * 2 (bc hex) = chars of offset e.g. 08 (value is 8, 2 chars in hex)
     offset=$((4 + offset + len * 2))
-    local next=$(echo -n $hexdata |cut -c "${offset}-$((offset + 1))")
+    local next
+    next=$(echo -n "$hexdata" |cut -c "${offset}-$((offset + 1))")
 
     # opt43 always ends with 0xff so if the next byte is ff it's the end for sure
-    if [ "x$next" == "xff" ] ; then
+    if [ "$next" = "ff" ] ; then
       break
     fi
   done
