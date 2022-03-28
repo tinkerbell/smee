83c6d05897341e49513647c4d1e6d867f04768a8
Nahum Shalman <nshalman@equinix.com>
2022-03-28 13:45:52 -0400

    Always pass conf.PublicFQDN to assemble HTTP URLs
    We saw an issue where DHCP responses from boots contained:
    http://:80/ipxe/undionly.kpxe
    
    We think this should fix that.
    
    Signed-off-by: Nahum Shalman <nshalman@equinix.com>

--- job/dhcp.go
+++ job/dhcp.go
@@ -165,7 +165,6 @@ func (j Job) setPXEFilename(rep *dhcp4.Packet, isTinkerbellIPXE, isARM, isUEFI, 
 	} else {
 		isHTTPClient = true
 		filename = "auto.ipxe"
-		j.HTTPServerFQDN = conf.PublicFQDN
 	}
 
 	if filename == "" {
@@ -175,5 +174,5 @@ func (j Job) setPXEFilename(rep *dhcp4.Packet, isTinkerbellIPXE, isARM, isUEFI, 
 		return
 	}
 
-	dhcp.SetFilename(rep, filename, j.NextServer, isHTTPClient, j.HTTPServerFQDN)
+	dhcp.SetFilename(rep, filename, j.NextServer, isHTTPClient, conf.PublicFQDN)
 }
